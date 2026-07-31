407956a8343379fa5a183502240dc773f143fbfd
bablubadmas125-cmyk <bablubadmas125@gmail.com>
2026-07-31 13:58:13 +0530

    Add responsive mobile navigation menu

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const navbar = document.querySelector(".navbar");
 
+menuToggle.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+});
